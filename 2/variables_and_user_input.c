df57a891babcc1c68c2484350a93f9fd10e9ca3d
5ilent5pring <162632271+5ilent5pring@users.noreply.github.com>
2025-01-04 00:33:06 +0300

    Create variables_and_user_input.c

---
+++ 2/variables_and_user_input.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    char analystName[50];
+    printf("Enter your name: ");
+    scanf("%s", analystName);
+    printf("Welcome to the Cyber Threat Intelligence Tool, %s!\n", analystName);
+    return 0;
+}
